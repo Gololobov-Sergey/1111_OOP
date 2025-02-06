6573771b259ba401a4889b0dacad495544db1b33
Gololobov-Sergey <gololobov.serhiy@gmail.com>
2025-02-06 17:49:42 +0200

    06.02.2025

---
+++ Sims8.py
@@ -0,0 +1,78 @@
+class Human:
+    def __init__(self, name, house, car=None, job=None):
+        self.name = name
+        self.house = house
+        self.car = car
+        self.job = job
+        self.money = 1000
+        self.gladness = 50
+
+    def shoping(self):
+        pass
+
+    def work(self):
+        pass
+
+    def eat(self):
+        pass
+
+    def chill(self):
+        pass
+
+    def cleaning(self):
+        pass
+
+    def info(self):
+        pass
+
+    def live(self):
+        pass
+
+    def is_alive(self):
+        if self.money < 0:
+            return False
+        return True
+
+class Car:
+    def __init__(self, model):
+        self.model = model
+        self.fuel = 60
+        self.state = 100
+
+    def drive(self, length):
+        rashod = length * 0.1
+        if self.fuel - rashod < 0:
+            print("Подорож не можлива, не вистачає пального")
+        else:
+            self.fuel -= rashod
+            self.state -= length * 0.01
+            print(f"Ми проїхали {length} км, витратили {rashod} л пального")
+
+    def add_fuel(self, fuel):
+        if self.fuel + fuel <= 60:
+            self.fuel += fuel
+            print(f"Ми заправили {fuel} л пального")
+        else:
+            self.fuel = 60
+            print("Бак повний!")
+
+    def __str__(self):
+        pass
+
+
+class Job:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def __str__(self):
+        return f"Робота: {self.name}, зарплатня - {self.salary}$"
+
+class House:
+    def __init__(self):
+        self.pollution = 0
+        self.food = 0
+
+    def __str__(self):
+        print(f"Будинок: запас їжі - {self.food}, забрудненність - {self.pollution}")
+
